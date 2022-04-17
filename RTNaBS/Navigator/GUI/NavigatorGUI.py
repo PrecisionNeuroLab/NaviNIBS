ac022f9bd0bcdfa175cefda23c7760a680b116d2
Chris Cline <chriscline@users.noreply.github.com>
2022-04-16 23:32:51 -0700

    Refactored for better modularity

--- RTNaBS/Navigator/GUI/NavigatorGUI.py
+++ RTNaBS/Navigator/GUI/NavigatorGUI.py
@@ -24,20 +24,36 @@ logger = logging.getLogger(__name__)
 
 @attrs.define()
 class _MainViewPanel:
-    _parent: NavigatorGUI
+    _session: tp.Optional[Session] = None
     _wdgt: QtWidgets.QWidget = attrs.field(init=False, factory=QtWidgets.QWidget)
 
     @property
     def wdgt(self):
         return self._wdgt
 
+    @property
+    def session(self):
+        return self._session
+
+    @session.setter
+    def session(self, newVal: tp.Optional[session]):
+        self._session = newVal
+        self._onSessionSet()
+
+    def _onSessionSet(self):
+        pass  # to be implemented by subclass
+
 
 @attrs.define()
 class _Panel_CreateOrLoadSession(_MainViewPanel):
 
+    _inProgressBaseDir: tp.Optional[str] = None
     _saveBtn: QtWidgets.QPushButton = attrs.field(init=False)
     _saveAsBtn: QtWidgets.QPushButton = attrs.field(init=False)
     _closeBtn: QtWidgets.QPushButton = attrs.field(init=False)
+
+    sigLoadedSession: Signal = attrs.field(init=False, factory=lambda: Signal((Session,)))
+    sigClosedSession: Signal = attrs.field(init=False, factory=lambda: Signal((Session,)))
 
     def __attrs_post_init__(self):
         self._wdgt.setLayout(QtWidgets.QGridLayout())
@@ -73,44 +89,43 @@ class _Panel_CreateOrLoadSession(_MainViewPanel):
         btn.clicked.connect(lambda checked: self._cloneSession())
         self._wdgt.layout().addWidget(btn, 3, 0)
 
-        self._parent.sigLoadedSession.connect(lambda session: self._updateEnabledBtns())
-        self._parent.sigClosedSession.connect(lambda session: self._updateEnabledBtns())
+        self._updateEnabledBtns()
 
+    def _onSessionSet(self):
         self._updateEnabledBtns()
 
     def _getNewInProgressSessionDir(self) -> str:
-        return os.path.join(self._parent._inProgressBaseDir, 'RTNaBSSession_' + datetime.today().strftime(
-                    '%y%m%d%H%M%S'))
+        return os.path.join(self._inProgressBaseDir, 'RTNaBSSession_' + datetime.today().strftime('%y%m%d%H%M%S'))
 
     def _updateEnabledBtns(self):
         for btn in (self._saveBtn, self._saveAsBtn, self._closeBtn):
-            btn.setEnabled(self._parent._session is not None)
+            btn.setEnabled(self.session is not None)
 
     def _saveSession(self):
-        self._parent._session.saveToFile()
+        self.session.saveToFile()
 
     def _saveSessionAs(self, sesFilepath: tp.Optional[str] = None):
         if sesFilepath is None:
-            prevFilepath = self._parent._session.filepath
-            sesFilepath, _ = QtWidgets.QFileDialog.getSaveFileName(self._parent._win,
-                                                                'Save session file',
-                                                                prevFilepath,
-                                                                'Session file (*.rtnabs)')
+            prevFilepath = self.session.filepath
+            sesFilepath, _ = QtWidgets.QFileDialog.getSaveFileName(self._wdgt,
+                                                                   'Save session file',
+                                                                   prevFilepath,
+                                                                   'Session file (*.rtnabs)')
             if len(sesFilepath) == 0:
                 logger.info('Browse save session cancelled')
                 return
         logger.info('New session filepath: {}'.format(sesFilepath))
-        self._parent._session.filepath = sesFilepath
+        self.session.filepath = sesFilepath
         self._saveSession()
 
     def _closeSession(self):
-        closedSession = self._parent._session
-        self._parent._session = None
-        self._parent.sigClosedSession.emit(closedSession)
+        closedSession = self.session
+        self.session = None
+        self.sigClosedSession.emit(closedSession)
 
     def _tryVerifyThenCloseSession(self):
-        if self._parent._session is not None:
-            if self._parent._session.compressedFileIsDirty:
+        if self.session is not None:
+            if self.session.compressedFileIsDirty:
                 raise NotImplementedError()  # TODO: prompt user to confirm whether they want to save or discard changes to previous session
                 # TODO: close session to trigger clearing of various GUI components
             self._closeSession()
@@ -124,7 +139,7 @@ class _Panel_CreateOrLoadSession(_MainViewPanel):
                 dir = 'todo'
             else:
                 dir = str(pathlib.Path.home())
-            sesFilepath, _ = QtWidgets.QFileDialog.getSaveFileName(self._parent._win,
+            sesFilepath, _ = QtWidgets.QFileDialog.getSaveFileName(self._wdgt,
                                                                 'Create new session file',
                                                                 dir,
                                                                 'Session file (*.rtnabs)')
@@ -133,8 +148,8 @@ class _Panel_CreateOrLoadSession(_MainViewPanel):
                 return
         logger.info('New session filepath: {}'.format(sesFilepath))
 
-        self._parent._session = Session.createNew(filepath=sesFilepath, unpackedSessionDir=self._getNewInProgressSessionDir())
-        self._parent.sigLoadedSession.emit(self._parent._session)
+        self.session = Session.createNew(filepath=sesFilepath, unpackedSessionDir=self._getNewInProgressSessionDir())
+        self.sigLoadedSession.emit(self.session)
 
     def loadSession(self, sesFilepath: tp.Optional[str] = None):
         self._tryVerifyThenCloseSession()
@@ -145,28 +160,28 @@ class _Panel_CreateOrLoadSession(_MainViewPanel):
                 dir = 'todo'
             else:
                 dir = str(pathlib.Path.home())
-            sesFilepath, _ = QtWidgets.QFileDialog.getOpenFileName(self._parent._win, 'Choose session to load', dir, 'Session file (*.rtnabs)')
+            sesFilepath, _ = QtWidgets.QFileDialog.getOpenFileName(self._wdgt, 'Choose session to load', dir, 'Session file (*.rtnabs)')
             if len(sesFilepath) == 0:
                 logger.info('Browse existing session cancelled')
                 return
         logger.info('Load session filepath: {}'.format(sesFilepath))
 
-        self._parent._session = Session.loadFromFile(filepath=sesFilepath, unpackedSessionDir=self._getNewInProgressSessionDir())
-        self._parent.sigLoadedSession.emit(self._parent._session)
+        self.session = Session.loadFromFile(filepath=sesFilepath, unpackedSessionDir=self._getNewInProgressSessionDir())
+        self.sigLoadedSession.emit(self.session)
 
     def _recoverSession(self, sesDataDir: tp.Optional[str] = None):
         self._tryVerifyThenCloseSession()
 
         if sesDataDir is None:
-            dir = self._parent._inProgressBaseDir
-            sesDataDir = QtWidgets.QFileDialog.getExistingDirectory(self._parent._win, 'Choose unpacked session to load', dir)
+            dir = self._inProgressBaseDir
+            sesDataDir = QtWidgets.QFileDialog.getExistingDirectory(self._wdgt, 'Choose unpacked session to load', dir)
             if len(sesDataDir) == 0:
                 logger.info('Browse recover session cancelled')
                 return
         logger.info('Recover session data dir: {}'.format(sesDataDir))
 
-        self._parent._session = Session.loadFromUnpackedDir(unpackedSessionDir=sesDataDir)
-        self._parent.sigLoadedSession.emit(self._parent._session)
+        self.session = Session.loadFromUnpackedDir(unpackedSessionDir=sesDataDir)
+        self.sigLoadedSession.emit(self.session)
 
     def _cloneSession(self, fromSesFilepath: tp.Optional[str] = None, toSesFilepath: tp.Optional[str] = None):
         if fromSesFilepath is None:
@@ -175,7 +190,7 @@ class _Panel_CreateOrLoadSession(_MainViewPanel):
                 dir = 'todo'
             else:
                 dir = str(pathlib.Path.home())
-            fromSesFilepath, _ = QtWidgets.QFileDialog.getOpenFileName(self._parent._win, 'Choose session to clone', dir,
+            fromSesFilepath, _ = QtWidgets.QFileDialog.getOpenFileName(self._wdgt, 'Choose session to clone', dir,
                                                                 "Session file (*.rtnabs)")
             if len(sesFilepath) == 0:
                 logger.info('Browse existing session cancelled')
@@ -185,7 +200,7 @@ class _Panel_CreateOrLoadSession(_MainViewPanel):
 
         if toSesFilepath is None:
             dir, _ = os.path.split(fromSesFilepath)
-            toSesFilepath, _ = QtWidgets.QFileDialog.getSaveFileName(self._parent._win, 'Create save cloned session file', dir, "Session file (*.rtnabs)")
+            toSesFilepath, _ = QtWidgets.QFileDialog.getSaveFileName(self._wdgt, 'Create save cloned session file', dir, "Session file (*.rtnabs)")
             if len(sesFilepath) == 0:
                 logger.info('Browse clone session cancelled')
                 return
@@ -206,38 +221,36 @@ class _Panel_SessionInfo(_MainViewPanel):
         self._wdgt.setLayout(QtWidgets.QFormLayout())
 
         wdgt = QtWidgets.QLineEdit()
-        wdgt.textChanged.connect(lambda text, key='subjectID': self._onTextChanged(key, text))
+        wdgt.textEdited.connect(lambda text, key='subjectID': self._onTextEdited(key, text))
         self._wdgts['subjectID'] = wdgt
         self._wdgt.layout().addRow('Subject ID', wdgt)
         # TODO: continue here
 
         wdgt = QtWidgets.QLineEdit()
-        wdgt.textChanged.connect(lambda text, key='sessionID': self._onTextChanged(key, text))
+        wdgt.textEdited.connect(lambda text, key='sessionID': self._onTextEdited(key, text))
         self._wdgts['sessionID'] = wdgt
         self._wdgt.layout().addRow('Session ID', wdgt)
 
-        self._parent.sigLoadedSession.connect(self._onSessionLoaded)
-        self._parent.sigClosedSession.connect(lambda _: self._onSessionInfoChanged())
-
-    def _onSessionLoaded(self, session: Session):
-        session.sigInfoChanged.connect(self._onSessionInfoChanged)
+    def _onSessionSet(self):
+        if self.session is not None:
+            self.session.sigInfoChanged.connect(self._onSessionInfoChanged)
         self._onSessionInfoChanged()
 
     def _onSessionInfoChanged(self):
-        if self._parent.session is None:
+        if self.session is None:
             for key in ('subjectID', 'sessionID'):
                 self._wdgts[key].setText('')
         else:
             for key in ('subjectID', 'sessionID'):
-                val = getattr(self._parent.session, key)
+                val = getattr(self.session, key)
                 self._wdgts[key].setText('' if val is None else val)
 
-    def _onTextChanged(self, key: str, text: str):
+    def _onTextEdited(self, key: str, text: str):
         if len(text) == 0:
             text = None
-        if self._parent.session is not None:
+        if self.session is not None:
             logger.info('Applying edited value of {} to session: {}'.format(key, text))
-            setattr(self._parent.session, key, text)
+            setattr(self.session, key, text)
         else:
             logger.warning('Ignoring edited value of {} since session is closed.'.format(key))
 
@@ -254,9 +267,6 @@ class NavigatorGUI(RunnableAsApp):
     _mainViewPanels: tp.Dict[str, _MainViewPanel] = attrs.field(init=False, factory=dict)
     _toolbarWdgt: QtWidgets.QToolBar = attrs.field(init=False)
     _toolbarBtnActions: tp.Dict[str, QtWidgets.QAction] = attrs.field(init=False, factory=dict)
-
-    sigLoadedSession: Signal = attrs.field(factory=lambda: Signal((Session,)))
-    sigClosedSession: Signal = attrs.field(factory=lambda: Signal((Session,)))
 
     def __attrs_post_init__(self):
         logger.info('Initializing {}'.format(self.__class__.__name__))
@@ -284,32 +294,33 @@ class NavigatorGUI(RunnableAsApp):
             self._toolbarBtnActions[key].setCheckable(True)
             self._toolbarBtnActions[key].triggered.connect(lambda checked=False, key=key: self._activateView(viewKey=key))
 
-        createViewPanel('New / load', _Panel_CreateOrLoadSession(parent=self), icon=qta.icon('mdi6.folder'))
+        panel = _Panel_CreateOrLoadSession(session=self._session,
+                                           inProgressBaseDir=self._inProgressBaseDir)
+        createViewPanel('New / load', panel, icon=qta.icon('mdi6.folder'))
+        panel.sigLoadedSession.connect(self._onSessionLoaded)
+        panel.sigClosedSession.connect(self._onSessionClosed)
 
-        createViewPanel('Session info', _Panel_SessionInfo(parent=self), icon=qta.icon('mdi6.form-select'))
+        createViewPanel('Session info', _Panel_SessionInfo(session=self._session), icon=qta.icon('mdi6.form-select'))
 
-        createViewPanel('Set MRI', _MainViewPanel(parent=self), icon=qta.icon('mdi6.image'))
+        createViewPanel('Set MRI', _MainViewPanel(session=self._session), icon=qta.icon('mdi6.image'))
         # TODO: set up MRI widget
 
-        createViewPanel('Set head model', _MainViewPanel(parent=self), icon=qta.icon('mdi6.head-cog-outline'))
+        createViewPanel('Set head model', _MainViewPanel(session=self._session), icon=qta.icon('mdi6.head-cog-outline'))
         # TODO: set up head model widget
 
-        createViewPanel('Set fiducials', _MainViewPanel(parent=self), icon=qta.icon('mdi6.head-snowflake-outline'))
+        createViewPanel('Set fiducials', _MainViewPanel(session=self._session), icon=qta.icon('mdi6.head-snowflake-outline'))
         # TODO: set up fiducials widget
 
-        createViewPanel('Set transforms', _MainViewPanel(parent=self), icon=qta.icon('mdi6.head-sync-outline'))
+        createViewPanel('Set transforms', _MainViewPanel(session=self._session), icon=qta.icon('mdi6.head-sync-outline'))
         # TODO: set up transforms widget
 
-        createViewPanel('Set targets', _MainViewPanel(parent=self), icon=qta.icon('mdi6.head-flash-outline'))
+        createViewPanel('Set targets', _MainViewPanel(session=self._session), icon=qta.icon('mdi6.head-flash-outline'))
         # TODO: set up targets widget
 
         # set initial view widget visibility
         # TODO: default to MRI if new session, otherwise default to something else...
         self._updateEnabledToolbarBtns()
         self._activateView('New / load')
-
-        self.sigLoadedSession.connect(self._onLoadedSession)
-        self.sigClosedSession.connect(self._onClosedSession)
 
         if self._sesFilepath is not None:
             asyncio.create_task(self._loadAfterSetup(filepath=self._sesFilepath))
@@ -318,6 +329,21 @@ class NavigatorGUI(RunnableAsApp):
             logger.debug('Showing window')
             self._win.show()
 
+    def _onSessionLoaded(self, session: Session):
+        assert session is not None
+        logger.info('Loaded session {}'.format(session.filepath))
+        self._session = session
+        for pane in self._mainViewPanels.values():
+            pane.session = session
+        self._updateEnabledToolbarBtns()
+
+    def _onSessionClosed(self, prevSession: Session):
+        logger.info('Closed session {}'.format(prevSession.filepath))
+        self._session = None
+        for pane in self._mainViewPanels.values():
+            pane.session = None
+        self._updateEnabledToolbarBtns()
+
     @property
     def session(self):
         return self._session
@@ -325,16 +351,6 @@ class NavigatorGUI(RunnableAsApp):
     async def _loadAfterSetup(self, filepath):
         await asyncio.sleep(1.)
         self._mainViewPanels['New / load'].loadSession(sesFilepath=filepath)
-
-    def _onLoadedSession(self, session: Session):
-        logger.info('Loaded session {}'.format(session.filepath))
-        assert self._session is not None
-        self._updateEnabledToolbarBtns()
-
-    def _onClosedSession(self, session: Session):
-        logger.info('Closed session {}'.format(session.filepath))
-        assert self._session is None
-        self._updateEnabledToolbarBtns()
 
     def _updateEnabledToolbarBtns(self):
 
