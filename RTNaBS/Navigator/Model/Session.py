c1998326c45077260de00ab5b81ca55c82640d1c
Chris Cline <chriscline@users.noreply.github.com>
2022-04-16 17:26:19 -0700

    More progress on GUI foundations

--- RTNaBS/Navigator/Model/Session.py
+++ RTNaBS/Navigator/Model/Session.py
@@ -1,32 +1,54 @@
 import attrs
+import tempfile
 import typing as tp
 
 
-@attrs.define
+@attrs.define()
 class MRI:
     pass
 
 
-@attrs.define
+@attrs.define()
 class MNIRegistration:
     pass
 
 
-@attrs.define
+@attrs.define()
 class SubjectRegistration:
     pass
 
 
-@attrs.define
+@attrs.define()
 class Target:
     pass
 
 
-@attrs.define
+@attrs.define()
 class Session:
+    filepath: str  # path to compressed session file
     subjectID: tp.Optional[str] = None
     sessionID: tp.Optional[str] = None
     MRI: tp.Optional[MRI] = None
     MNIRegistration: tp.Optional[MNIRegistration] = None
     SubjectRegistration: tp.Optional[SubjectRegistration] = None
     targets: tp.Dict[str, Target] = None
+
+    sessionConfigFilename: str = 'SessionConfig.json'
+    _unpackedSessionDir: tempfile.TemporaryDirectory = attrs.field(init=False, default=None)
+
+    def __attrs_post_init__(self):
+
+
+
+        raise NotImplementedError()  # TODO
+
+    def saveToUnpackedDir(self, saveDirtyOnly: bool = True):
+        raise NotImplementedError()  # TODO
+
+    @classmethod
+    def loadFromFile(cls, sessionFilepath: str, unpackToDir: str):
+        raise NotImplementedError()  # TODO
+
+    @classmethod
+    def loadFromUnpackedDir(cls, unpackedSessionDir: str):
+        raise NotImplementedError()  # TODO
