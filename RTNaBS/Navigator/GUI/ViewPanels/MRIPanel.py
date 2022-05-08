40e30ea0c78cd73668d35fa5a982d21a91f7ef5e
Chris Cline <chriscline@users.noreply.github.com>
2022-05-07 21:58:20 -0700

    Further improvements to MRI visualization and interaction

--- RTNaBS/Navigator/GUI/ViewPanels/MRIPanel.py
+++ RTNaBS/Navigator/GUI/ViewPanels/MRIPanel.py
@@ -6,6 +6,8 @@ import appdirs
 import attrs
 from datetime import datetime
 import logging
+import nibabel as nib
+from nibabel.affines import apply_affine
 import numpy as np
 import os
 import pathlib
@@ -30,6 +32,7 @@ class MRIPanel(MainViewPanel):
     _filepathWdgt: QFileSelectWidget = attrs.field(init=False)
     _plotters: tp.Dict[str, pvqt.QtInteractor] = attrs.field(init=False, factory=dict)
     _plottersInitialized: bool = attrs.field(init=False, default=False)
+    _lineActors: tp.Dict[str, pv.Line] = attrs.field(init=False, factory=dict)
     _sliceOrigin: tp.Optional[np.ndarray] = None
 
     def __attrs_post_init__(self):
@@ -52,7 +55,7 @@ class MRIPanel(MainViewPanel):
                 app=QtWidgets.QApplication.instance()
             )
             # TODO: maybe connect close signal to plotter
-            self._plotters[key].set_background(color='#DDDDDD')
+            self._plotters[key].set_background(color='#000000')
             containerLayout.addWidget(self._plotters[key].interactor, iRow, iCol)
 
     def _onSessionSet(self):
@@ -74,6 +77,18 @@ class MRIPanel(MainViewPanel):
 
     def _onSlicePointChanged(self, key: str):
         pos = self._plotters[key].picked_point
+        logger.debug('Slice point changed: {} {}'.format(key, pos))
+        if True:
+            # ignore any out-of-plane offset that can be caused by slice rendering differences
+            pos['xyz'.index(key)] = self._sliceOrigin['xyz'.index(key)]
+        self._sliceOrigin = pos
+        self._updateImagePreview()
+
+    def _onSliceScrolled(self, key, change: int):
+        logger.debug('Slice scrolled: {} {}'.format(key, change))
+        offset = np.zeros((3,))
+        offset['xyz'.index(key)] = change
+        pos = self._sliceOrigin + offset
         self._sliceOrigin = pos
         self._updateImagePreview()
 
@@ -94,46 +109,96 @@ class MRIPanel(MainViewPanel):
         if self._sliceOrigin is None:
             self._sliceOrigin = (self.session.MRI.data.affine @ np.append(np.asarray(self.session.MRI.data.shape)/2, 1))[:-1]
 
+        def _onMouseEvent(obj, event, axisKey: str):
+            if event == 'MouseWheelForwardEvent':
+                logger.debug('MouseWheelForwardEvent')
+                self._onSliceScrolled(key=axisKey, change=1)
+
+            elif event == 'MouseWheelBackwardEvent':
+                logger.debug('MouseWheelBackwardEvent')
+                self._onSliceScrolled(key=axisKey, change=-1)
+
         for iKey, key in enumerate(('x', 'y', 'z')):
-            slice = self.session.MRI.dataAsUniformGrid.slice(normal=key, origin=self._sliceOrigin)
-            self._plotters[key].add_mesh(slice,
-                                         name='slice',
-                                         cmap='gray')
-            self._plotters[key].camera_position = 'xyz'.replace(key, '')
-            self._plotters[key].enable_image_style()
+            logger.debug('Updating plot for {} slice'.format(key))
             if not self._plottersInitialized:
+                self._plotters[key].enable_parallel_projection()
                 self._plotters[key].enable_point_picking(left_clicking=True,
                                                          show_message=False,
                                                          show_point=False,
-                                                         callback=lambda newPt, key=key: self._onSlicePointChanged(key))
+                                                         callback=lambda newPt, key=key: self._onSlicePointChanged(
+                                                             key))
+                self._plotters[key].enable_image_style()
+                for event in ('MouseWheelForwardEvent', 'MouseWheelBackwardEvent'):
+                    self._plotters[key].iren._style_class.AddObserver(event, lambda obj, event, axisKey=key: _onMouseEvent(obj, event, axisKey=axisKey))
+
+            if False:
+                slice = self.session.MRI.dataAsUniformGrid.slice(normal=key, origin=self._sliceOrigin)
+                self._plotters[key].add_mesh(slice,
+                                             name='slice',
+                                             cmap='gray')
+                self._plotters[key].camera_position = 'xyz'.replace(key, '')
+            else:
+                # volume plotting with camera clipping
+                if not self._plottersInitialized:
+                    vol = self.session.MRI.dataAsUniformGrid
+                    self._plotters[key].add_volume(vol,
+                                                   scalars='MRI',
+                                                   name='MRI',
+                                                   mapper='gpu',
+                                                   clim=[300, 2000],
+                                                   cmap='gray')
 
         if not self._plottersInitialized:
+            logger.debug('Initializing 3D plot')
             self._plotters['3D'].add_volume(self.session.MRI.dataAsUniformGrid.gaussian_smooth(),
+                                     scalars='MRI',
                                      name='vol',
-                                     cmap='bone',
-                                     opacity='geom',
-                                     shade=True)
+                                     clim=[300, 1000],
+                                     cmap='gray',
+                                     mapper='gpu',
+                                     opacity=[0, 1, 1],
+                                     shade=False)
 
         for key in ('x', 'y', 'z', '3D'):
+            logger.debug('Setting crosshairs for {} plot'.format(key))
             lineLength = 300  # TODO: scale by image size
-            pts = None
             if key == '3D':
                 crosshairAxes = 'xyz'
-                centerGapLength = 10  # TODO: scale by image size
+                centerGapLength = 0
             else:
                 crosshairAxes = 'xyz'.replace(key, '')
-                centerGapLength = 0
+                centerGapLength = 10  # TODO: scale by image size
             for axis in crosshairAxes:
                 mask = np.zeros((1, 3))
                 mask[0, 'xyz'.index(axis)] = 1
-                newPts = np.asarray([centerGapLength/2, lineLength])[:, np.newaxis] * mask
-                newPts = np.concatenate((newPts, -1 * newPts), axis=0)
-                newPts += self._sliceOrigin
-                if pts is None:
-                    pts = newPts
+                for iDir, dir in enumerate((-1, 1)):
+                    pts = dir*np.asarray([centerGapLength/2, lineLength])[:, np.newaxis] * mask + self._sliceOrigin
+                    lineKey = 'Crosshair_{}_{}_{}'.format(key, axis, iDir)
+                    if not self._plottersInitialized:
+                        line = self._plotters[key].add_lines(pts, color='#11DD11', width=2, name=lineKey)
+                        self._lineActors[lineKey] = line
+                    else:
+                        logger.debug('Moving previous crosshairs')
+                        line = self._lineActors[lineKey]
+                        pts_pv = pv.lines_from_points(pts)
+                        line.GetMapper().SetInputData(pts_pv)
+
+            if key != '3D':
+                offsetDir = np.zeros((3,))
+                offsetDir['xyz'.index(key)] = 1
+                if True:
+                    self._plotters[key].camera.position = offsetDir * 100 + self._sliceOrigin
                 else:
-                    pts = np.concatenate((pts, newPts), axis=0)
-            self._plotters[key].add_lines(pts, color='#11DD11', width=2, name='Crosshair')
+                    # hack to prevent resetting clipping range due to pyvista implementation quirk
+                    tmp = self._plotters[key].camera._renderer
+                    self._plotters[key].camera._renderer = None
+                    self._plotters[key].camera.position = offsetDir * 100 + self._sliceOrigin
+                    self._plotters[key].camera._renderer = tmp
+
+                self._plotters[key].camera.focal_point = self._sliceOrigin
+                self._plotters[key].camera.up = np.roll(offsetDir, (2, 1, 2)['xyz'.index(key)])
+                self._plotters[key].camera.clipping_range = (99, 102)
+                self._plotters[key].camera.parallel_scale = 90
 
         self._plottersInitialized = True
 
