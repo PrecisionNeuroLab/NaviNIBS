17886c5fb94d06b35990bdd79273f34e142239c1
Chris Cline <chriscline@users.noreply.github.com>
2022-06-11 21:30:16 -0700

    Moved navigation ViewLayers to separate files

---
+++ RTNaBS/Navigator/GUI/ViewPanels/NavigatePanel/ViewLayers/MeshSurfaceLayer.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import asyncio
+
+import attrs
+import logging
+import numpy as np
+import typing as tp
+from typing import ClassVar
+
+from . import PlotViewLayer
+from RTNaBS.util.pyvista import Actor, setActorUserTransform, addLineSegments, concatenateLineSegments
+
+
+logger = logging.getLogger(__name__)
+
+
+Transform = np.ndarray
+
+
+@attrs.define
+class MeshSurfaceLayer(PlotViewLayer):
+    _type: ClassVar[str] = 'MeshSurface'
+    _color: str = '#d9a5b2'
+    _opacity: float = 0.7
+    _surfKey: str = 'gmSurf'
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+
+    def _redraw(self, which: tp.Union[tp.Optional[str], tp.List[str, ...]] = None):
+        super()._redraw(which=which)
+
+        if not isinstance(which, str):
+            # assume parent call above triggered appropriate redraws
+            return
+
+        if which == 'all':
+            which = ['initSurf']
+            self._redraw(which=which)
+            return
+
+        if which == 'initSurf':
+            mesh = getattr(self._coordinator.session.headModel, self._surfKey)
+
+            actorKey = self._getActorKey('surf')
+
+            self._actors[actorKey] = self._plotter.add_mesh(mesh=mesh,
+                                                            color=self._color,
+                                                            opacity=self._opacity,
+                                                            specular=0.5,
+                                                            diffuse=0.5,
+                                                            ambient=0.5,
+                                                            smooth_shading=True,
+                                                            split_sharp_edges=True,
+                                                            name=actorKey)
+
+            self._redraw('updatePosition')
+
+        elif which == 'updatePosition':
+            if self._plotInSpace == 'MRI':
+                if False:
+                    actorKey = self._getActorKey('surf')
+                    transf = np.eye(4)
+                    setActorUserTransform(self._actors[actorKey], transf)
+                else:
+                    pass  # assume since plotInSpace is always MRI (for now) that we don't need to update anything
+        else:
+            raise NotImplementedError
---
+++ RTNaBS/Navigator/GUI/ViewPanels/NavigatePanel/ViewLayers/__init__.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import attrs
+import logging
+import numpy as np
+import pyvista as pv
+import pyvistaqt as pvqt
+import qtawesome as qta
+from qtpy import QtWidgets, QtGui, QtCore
+import typing as tp
+from typing import ClassVar
+
+from RTNaBS.Navigator.GUI.ViewPanels.NavigatePanel.NavigationView import TargetingCoordinator
+from RTNaBS.util.pyvista import Actor, setActorUserTransform, addLineSegments, concatenateLineSegments
+
+
+logger = logging.getLogger(__name__)
+
+
+Transform = np.ndarray
+
+
+@attrs.define
+class ViewLayer:
+    _key: str
+    _type: ClassVar[str]
+    _coordinator: TargetingCoordinator
+
+    def __attrs_post_init__(self):
+        pass
+
+
+@attrs.define
+class PlotViewLayer(ViewLayer):
+    _plotter: pvqt.QtInteractor  # note this this one plotter may be shared between multiple ViewLayers
+    _plotInSpace: str = 'MRI'
+
+    _actors: tp.Dict[str, tp.Optional[Actor]] = attrs.field(init=False, factory=dict)
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self._redraw('all')
+
+    def _redraw(self, which: tp.Union[tp.Optional[str], tp.List[str, ...]] = None):
+
+        #logger.debug('redraw {}'.format(which))
+
+        if which is None:
+            which = 'all'
+
+        if not isinstance(which, str):
+            for subWhich in which:
+                self._redraw(which=subWhich)
+            return
+
+    def _getActorKey(self, subKey: str) -> str:
+        return self._key + '_' + subKey  # make actor keys unique across multiple layers in the same plotter
