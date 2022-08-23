445faabcc7037f47c28e346f8a29dcecd3e143fb
Chris Cline <chriscline@users.noreply.github.com>
2022-08-23 18:04:40 -0500

    Misc minor tweaks

--- RTNaBS/Devices/SimulatedToolPositionsClient.py
+++ RTNaBS/Devices/SimulatedToolPositionsClient.py
@@ -14,12 +14,21 @@ logger = logging.getLogger(__name__)
 
 @attrs.define
 class SimulatedToolPositionsClient(ToolPositionsClient):
+
+    _readyToCheckType: asyncio.Event = attrs.field(init=False, factory=asyncio.Event)
+
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
+        self.sigIsConnectedChanged.connect(lambda: self._readyToCheckType.set() if self._isConnected else self._readyToCheckType.clear())
 
-        serverType = self._connector.get('type')
-        if serverType != 'Simulated':
-            raise RuntimeError('Tried to use SimulatedToolPositionsClient to connect to non-simulated ToolPositionsServer')
+    async def _checkServerType(self):
+        while True:
+            await self._readyToCheckType.wait()
+            self._readyToCheckType.clear()
+
+            serverType = self._connector.get('type')
+            if serverType != 'Simulated':
+                logger.error('Tried to use SimulatedToolPositionsClient to connect to non-simulated ToolPositionsServer')
 
     async def setNewPosition_async(self, key: str, transf: tp.Optional[np.ndarray]):
         return await self._connector.callAsync_async('setNewPosition', key=key, transf=transf.tolist() if transf is not None else None)
