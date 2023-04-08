aaca1669a098fdc2218c9bc8cdc64c4c0791380f
Chris Cline <chriscline@users.noreply.github.com>
2023-04-08 01:25:06 -0500

    Improved async exception logging

--- RTNaBS/util/Asyncio.py
+++ RTNaBS/util/Asyncio.py
@@ -7,6 +7,26 @@ from . import exceptionToStr
 logger = logging.getLogger(__name__)
 
 
+async def asyncWaitWithCancel(
+        aws: tp.Iterable[tp.Awaitable],
+        timeout: float | None = None,
+        return_when=asyncio.ALL_COMPLETED) -> tuple[set[asyncio.Task], set[asyncio.Task]]:
+
+    done, pending = await asyncio.wait(aws, timeout=timeout, return_when=return_when)
+    for task in pending:
+        task.cancel()
+    cancelled = pending
+    return done, cancelled
+
+
+async def asyncTryAndLogExceptionOnError(fn: tp.Callable[..., tp.Awaitable], *args, **kwargs):
+    try:
+        return await fn(*args, **kwargs)
+    except Exception as e:
+        logger.error('Exception: %s' % exceptionToStr(e))
+        raise e
+
+
 def asyncioRunAndHandleExceptions(fn: tp.Callable[..., tp.Awaitable], *args, **kwargs):
     def handleExceptionInAsyncTask(loop: asyncio.AbstractEventLoop, context: dict):
         from pprint import pprint
