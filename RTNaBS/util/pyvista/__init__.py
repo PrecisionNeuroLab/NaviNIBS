fbd1a26deff2490f11ba515db1362c336978c5bd
Chris Cline <chriscline@users.noreply.github.com>
2023-05-15 21:17:17 -0500

    Add support for creating line segments colored according to a scalars field

--- RTNaBS/util/pyvista/__init__.py
+++ RTNaBS/util/pyvista/__init__.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyvista as pv
+from pyvista.plotting.mapper import make_mapper
 import typing as tp
 
 Actor = pv._vtk.vtkActor
@@ -16,9 +17,16 @@ def getActorUserTransform(actor: Actor) -> np.ndarray:
 
 
 def addLineSegments(plotter: pv.BasePlotter, lines: pv.PolyData,
-                    color='w', opacity=1., width=5,
+                    color='w',
+                    opacity=1.,
+                    width=5,
                     reset_camera: bool = False,
-                    label=None, name=None) -> Actor:
+                    label=None,
+                    name=None,
+                    scalar_bar_args: dict | None = None,
+                    scalars: str | None = None,
+                    show_scalar_bar: bool | None = None,
+                    cmap: str | tp.Any | None = None) -> Actor:
     """
     Similar to actor.add_lines but with a few improvements:
     - Allows `lines` arg to already be pv.PolyData (e.g. to already been passed through `pv.lines_from_points`.) This
@@ -29,18 +37,63 @@ def addLineSegments(plotter: pv.BasePlotter, lines: pv.PolyData,
     assert isinstance(lines, pv.PolyData)
 
     # Create mapper and add lines
-    mapper = pv._vtk.vtkDataSetMapper()
+    mapper = make_mapper(pv._vtk.vtkDataSetMapper)
     mapper.SetInputData(lines)
 
-    rgb_color = pv.colors.Color(color)
+    if scalars is None:
+        assert scalar_bar_args is None
+        rgb_color = pv.colors.Color(color)
+    else:
+        if show_scalar_bar is None:
+            show_scalar_bar = plotter._theme.show_scalar_bar
+
+        n_colors = 256
+
+        scalar_bar_args = scalar_bar_args.copy()
+        scalar_bar_args.setdefault('n_colors', n_colors)
+        scalar_bar_args['mapper'] = mapper
+
+        nan_color = pv.Color(None, default_opacity=1., default_color=plotter._theme.nan_color)
+
+        if isinstance(scalars, str):
+            scalars = pv.utilities.helpers.get_array(lines, scalars, preference='points')
+
+        clim = [np.nanmin(scalars), np.nanmax(scalars)]
+
+        if any(np.isnan(x) for x in clim):
+            # autoset clim within set_scalars errors if all values are nan
+            clim = [0, 1]
+
+        mapper.set_scalars(lines,
+                           scalars=scalars,
+                           scalar_bar_args=scalar_bar_args,
+                           rgb=False,
+                           component=None,
+                           preference='points',
+                           interpolate_before_map=True,
+                           _custom_opac=False,
+                           annotations=None,
+                           log_scale=False,
+                           nan_color=nan_color,
+                           above_color=None,
+                           below_color=None,
+                           cmap=cmap,
+                           flip_scalars=False,
+                           opacity=opacity,
+                           categories=False,
+                           n_colors=n_colors,
+                           clim=clim,
+                           theme=plotter._theme,
+                           show_scalar_bar=show_scalar_bar)
 
     # Create actor
     actor = pv._vtk.vtkActor()
     actor.SetMapper(mapper)
     actor.GetProperty().SetLineWidth(width)
     actor.GetProperty().EdgeVisibilityOn()
-    actor.GetProperty().SetEdgeColor(rgb_color.float_rgb)
-    actor.GetProperty().SetColor(rgb_color.float_rgb)
+    if scalars is None:
+        actor.GetProperty().SetEdgeColor(rgb_color.float_rgb)
+        actor.GetProperty().SetColor(rgb_color.float_rgb)
     actor.GetProperty().LightingOff()
     actor.GetProperty().SetOpacity(opacity)
 
@@ -50,6 +103,9 @@ def addLineSegments(plotter: pv.BasePlotter, lines: pv.PolyData,
             raise TypeError('Label must be a string')
         addr = actor.GetAddressAsString("")
         plotter.renderer._labels[addr] = [lines, label, rgb_color]
+
+    if show_scalar_bar and scalars is not None:
+        plotter.add_scalar_bar(**scalar_bar_args)
 
     # Add to renderer
     plotter.add_actor(actor, reset_camera=reset_camera, name=name, pickable=False)
