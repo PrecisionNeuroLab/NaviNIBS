f5cd5eaf1e8de8f5160176f1cecd7c739647bef6
chriscline-shared <46902449+chriscline-shared@users.noreply.github.com>
2023-07-11 14:17:13 -0700

    Misc minor fixes

--- RTNaBS/Navigator/GUI/ViewPanels/TriggerSettingsPanel/LSLTriggerSourceSettingsWidget.py
+++ RTNaBS/Navigator/GUI/ViewPanels/TriggerSettingsPanel/LSLTriggerSourceSettingsWidget.py
@@ -115,7 +115,11 @@ class LSLTriggerSourceSettingsWidget(TriggerSourceSettingsWidget[LSLTriggerSourc
                 if action is None:
                     action = self.triggerSource.defaultAction
 
-                metadata = dict(originalType=int(evtDat))
+                try:
+                    metadata = dict(originalType=int(evtDat))
+                except ValueError as e:
+                    metadata = dict()
+
                 if self.triggerSource.triggerValueIsEpochID:
                     metadata['epochID'] = int(evtDat)
                     # TODO: assert that epochID is actually unique (to catch issues where the selected stream
