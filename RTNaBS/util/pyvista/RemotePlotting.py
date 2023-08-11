7ac7cdcbd5a50621daf31296874106a793ff6950
Chris Cline <chriscline@users.noreply.github.com>
2023-08-11 13:30:29 -0500

    Solved bug with remote plotting deadlock

--- RTNaBS/util/pyvista/RemotePlotting.py
+++ RTNaBS/util/pyvista/RemotePlotting.py
@@ -7,6 +7,7 @@ from multiprocessing import connection as mpc
 import numpy as np
 import pyvista as pv
 import pyvistaqt as pvqt
+import time
 import vtkmodules.vtkRenderingAnnotation
 from qtpy import QtGui, QtCore, QtWidgets
 import typing as tp
@@ -16,10 +17,8 @@ import zmq.asyncio as azmq
 from RTNaBS.util import ZMQAsyncioFix, exceptionToStr
 from RTNaBS.util.Asyncio import asyncTryAndLogExceptionOnError
 from RTNaBS.util.GUI.QAppWithAsyncioLoop import RunnableAsApp
-if True:
-    from RTNaBS.util.pyvista.plotting import BackgroundPlotter
-else: # TODO: debug, delete
-    from pyvistaqt.plotting import BackgroundPlotter
+from RTNaBS.util.pyvista.plotting import BackgroundPlotter
+from pyvistaqt.plotting import BackgroundPlotter as PVQTBackgroundPlotter
 from RTNaBS.util.pyvista import Actor
 
 logger = logging.getLogger(__name__)
@@ -49,12 +48,6 @@ class RemoteActor:
 
     def GetVisibility(self) -> bool:
         import time
-        time.sleep(0.01)  # TODO: debug, delete
-        #return True  # TODO: debug, delete
-        if True:  #TODO: debug, delete or set to False
-            self.plotter._remoteCall('noop')
-            return True
-
         return self.plotter._remoteActorCall(self, 'GetVisibility')
 
 
@@ -74,45 +67,25 @@ class _ActorManager:
         return self._actors[actorRef]
 
 
-@attrs.define
-class _RemotePlotManager:
-    _reqPort: int | mpc.PipeConnection
+@attrs.define(kw_only=True)
+class _RemotePlotManagerBase:
     _parentLayout: QtWidgets.QLayout
-    _repPort: int | mpc.PipeConnection | None = None
-    _addr: str = '127.0.0.1'
     _plotterArgs: list = attrs.field(factory=list)
     _plotterKwargs: dict = attrs.field(factory=dict)
 
     _actorManager: _ActorManager = attrs.field(init=False, factory=_ActorManager)
-    _plotter: _RemotePlotter | None = attrs.field(init=False, default=None)
-    _ctx: azmq.Context = attrs.field(init=False, factory=azmq.Context)
-    _reqSock: azmq.Socket | mpc.PipeConnection = attrs.field(init=False)
-    _repSock: azmq.Socket | mpc.PipeConnection = attrs.field(init=False)
-    _socketLoopTask: asyncio.Task = attrs.field(init=False)
+    _Plotter: tp.ClassVar
+    _plotter: _RemotePlotter | _RemotePlotterNoAsync | None = attrs.field(init=False, default=None)
 
     def __attrs_post_init__(self):
-        if not isinstance(self._reqPort, mpc.PipeConnection):
-            self._reqSock = self._ctx.socket(zmq.REQ)
-            self._reqSock.connect(f'tcp://{self._addr}:{self._reqPort}')
-
-            self._repSock = self._ctx.socket(zmq.REP)
-            if self._repPort is None:
-                self._repPort = self._repSock.bind_to_random_port(f'tcp://{self._addr}')
-            else:
-                self._repSock.bind(f'tcp://{self._addr}:{self._repPort}')
-        else:
-            assert isinstance(self._reqPort, mpc.PipeConnection)
-            self._repSock = self._repPort
-            self._reqSock = self._reqPort
-
-        self._socketLoopTask = asyncio.create_task(asyncTryAndLogExceptionOnError(self._socketLoop))
+        pass
 
     def initPlotter(self):
         # don't do this automatically to allow deferred widget instantiation
         assert self._plotter is None
         logger.info('Initializing plotter')
-        self._plotter = _RemotePlotter(*self._plotterArgs,
-                                       parent=self._parentLayout.parentWidget(),
+        self._plotter = self._Plotter(*self._plotterArgs,
+                                       #parent=self._parentLayout.parentWidget(),
                                        **self._plotterKwargs)
         logger.debug('Adding plotter to parent layout')
         self._parentLayout.addWidget(self._plotter)
@@ -135,8 +108,6 @@ class _RemotePlotManager:
         args = list(msg[1])
         kwargs = msg[2]
 
-        self._plotter._renderNow()  # TODO: debug, delete
-
         return self._callMethod(fn, args, kwargs)
 
     def _callMethod(self, fn, args, kwargs):
@@ -157,6 +128,63 @@ class _RemotePlotManager:
             result = actorRef
 
         return result
+
+
+class _RemotePlotterNoAsync(PVQTBackgroundPlotter):
+    def __init__(self, *args, **kwargs):
+        PVQTBackgroundPlotter.__init__(self, *args,
+                                   **kwargs)
+
+    def setActorUserTransform(self, actor: Actor, transform: np.ndarray):
+        # convert from numpy array to vtkTransform
+        t = pv._vtk.vtkTransform()
+        t.SetMatrix(pv.vtkmatrix_from_array(transform))
+
+        actor.SetUserTransform(t)
+
+
+class _RemotePlotter(BackgroundPlotter):
+    def __init__(self, *args, **kwargs):
+        BackgroundPlotter.__init__(self, *args,
+                                   **kwargs)
+
+    def setActorUserTransform(self, actor: Actor, transform: np.ndarray):
+        # convert from numpy array to vtkTransform
+        t = pv._vtk.vtkTransform()
+        t.SetMatrix(pv.vtkmatrix_from_array(transform))
+
+        actor.SetUserTransform(t)
+
+
+@attrs.define(kw_only=True)
+class _RemotePlotManager(_RemotePlotManagerBase):
+    _reqPort: int | mpc.PipeConnection
+    _repPort: int | mpc.PipeConnection | None = None
+    _addr: str = '127.0.0.1'
+    _Plotter: tp.ClassVar = _RemotePlotter
+    _ctx: azmq.Context = attrs.field(init=False, factory=azmq.Context)
+    _reqSock: azmq.Socket | mpc.PipeConnection = attrs.field(init=False)
+    _repSock: azmq.Socket | mpc.PipeConnection = attrs.field(init=False)
+    _socketLoopTask: asyncio.Task = attrs.field(init=False)
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+
+        if not isinstance(self._reqPort, mpc.PipeConnection):
+            self._reqSock = self._ctx.socket(zmq.REQ)
+            self._reqSock.connect(f'tcp://{self._addr}:{self._reqPort}')
+
+            self._repSock = self._ctx.socket(zmq.REP)
+            if self._repPort is None:
+                self._repPort = self._repSock.bind_to_random_port(f'tcp://{self._addr}')
+            else:
+                self._repSock.bind(f'tcp://{self._addr}:{self._repPort}')
+        else:
+            assert isinstance(self._reqPort, mpc.PipeConnection)
+            self._repSock = self._repPort
+            self._reqSock = self._reqPort
+
+        self._socketLoopTask = asyncio.create_task(asyncTryAndLogExceptionOnError(self._socketLoop))
 
     async def _socketLoop(self):
 
@@ -217,11 +245,6 @@ class _RemotePlotManager:
                     await sendResponse('ack')
 
                 case 'callActorMethod':
-                    if True:  # TODO: debug, delete or set to False
-                        result = True
-                        await sendResponse(result)
-                        continue
-
                     try:
                         assert isinstance(msg[1], ActorRef)
                         result = self._callActorMethod(msg[1], msg[2:])
@@ -245,18 +268,99 @@ class _RemotePlotManager:
                         await sendResponse(result)
 
 
-class _RemotePlotter(BackgroundPlotter):
-    def __init__(self, *args, **kwargs):
-        BackgroundPlotter.__init__(self, *args,
-                                   #off_screen=True,  # TODO: debug, delete
-                                   **kwargs)
 
-    def setActorUserTransform(self, actor: Actor, transform: np.ndarray):
-        # convert from numpy array to vtkTransform
-        t = pv._vtk.vtkTransform()
-        t.SetMatrix(pv.vtkmatrix_from_array(transform))
 
-        actor.SetUserTransform(t)
+@attrs.define(kw_only=True)
+class _RemotePlotManagerNoAsync(_RemotePlotManagerBase):
+    _reqConn: mpc.PipeConnection
+    _parentLayout: QtWidgets.QLayout
+    _repConn: mpc.PipeConnection
+    _Plotter: tp.ClassVar = PVQTBackgroundPlotter
+    _connLoopTimer: QtCore.QTimer = attrs.field(init=False)
+    _connLoopGenerator: tp.Generator = attrs.field(init=False)
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+
+        self._connLoopGenerator = self._connLoop()
+
+        self._connLoopTimer = QtCore.QTimer(self._parentLayout.parentWidget())
+        self._connLoopTimer.timeout.connect(lambda: next(self._connLoopGenerator))
+        self._connLoopTimer.setInterval(100)
+        self._connLoopTimer.start()
+
+    def _test(self):
+        self._connLoop()
+
+    def _connLoop(self):
+        self._reqConn.send('ready')
+        while not self._reqConn.poll():
+            yield
+        resp = self._reqConn.recv()
+        assert resp == 'ack'
+
+        while True:
+            logger.debug('Awaiting msg')
+            while not self._repConn.poll():
+                yield
+            msg = self._repConn.recv()
+            logger.debug(f'Received msg')
+            logger.debug(f'Received msg: {msg}')
+
+            def sendResponse(response):
+                self._repConn.send(response)
+
+            if not isinstance(msg, tuple):
+                sendResponse(NotImplementedError('Unexpected message format'))
+                continue
+
+            match msg[0]:
+                case 'noop':
+                    sendResponse('ack')
+
+                case 'getWinID':
+                    if True:
+                        win = self._parentLayout.parentWidget().window()
+                    else:
+                        win = self._plotter.window()
+                    sendResponse(win.winId())
+
+                case 'showWindow':
+                    if True:
+                        win = self._parentLayout.parentWidget().window()
+                    else:
+                        win = self._plotter.window()
+
+                    if self._plotter is None:
+                        # assume everything else is now ready and we should instantiate plotter
+                        self.initPlotter()
+
+                    win.show()
+                    sendResponse('ack')
+
+                case 'callActorMethod':
+                    try:
+                        assert isinstance(msg[1], ActorRef)
+                        result = self._callActorMethod(msg[1], msg[2:])
+
+                    except Exception as e:
+                        logger.error(f'Error calling actor method: {exceptionToStr(e)}')
+                        sendResponse(e)
+
+                    else:
+                        sendResponse(result)
+
+                case _:
+                    try:
+                        result = self._callPlotMethod(msg)
+
+                    except Exception as e:
+                        logger.error(f'Error calling plot method {msg[0]}: {exceptionToStr(e)}')
+                        sendResponse(e)
+
+                    else:
+                        sendResponse(result)
+
 
 
 @attrs.define(kw_only=True)
@@ -265,7 +369,6 @@ class _RemotePlotterApp(RunnableAsApp):
     _repPort: int | mpc.PipeConnection | None = None
     _appName: str = 'RemotePlotter'
     _plotterKwargs: dict = attrs.field(factory=dict)
-    _appLogEveryNLoops: tp.Optional[int] = 1  # TODO: debug, delete
 
     _plotManager: _RemotePlotManager = attrs.field(init=False)
     _rootWdgt: QtWidgets.QWidget = attrs.field(init=False)
@@ -291,13 +394,49 @@ class _RemotePlotterApp(RunnableAsApp):
         flags |= QtCore.Qt.SubWindow
         self._win.setWindowFlags(flags)
 
-        self._debugTimer = QtCore.QTimer()
-        self._debugTimer.setInterval(1)
-        self._debugTimer.timeout.connect(self._debugTimerTimeout)
-        self._debugTimer.start()
 
-    def _debugTimerTimeout(self):
-        logger.debug('Timer tick')
+
+@attrs.define(kw_only=True)
+class _RemotePlotterAppNoAsync:
+    _reqPort: int | mpc.PipeConnection
+    _repPort: int | mpc.PipeConnection | None = None
+    _appName: str = 'RemotePlotter'
+    _plotterKwargs: dict = attrs.field(factory=dict)
+
+    _win: QtWidgets.QMainWindow = attrs.field(init=False, factory=QtWidgets.QMainWindow)
+    _plotManager: _RemotePlotManagerNoAsync = attrs.field(init=False)
+    _rootWdgt: QtWidgets.QWidget = attrs.field(init=False)
+    _debugTimer: QtCore.QTimer = attrs.field(init=False)
+
+    def __attrs_post_init__(self):
+        wdgt = QtWidgets.QWidget()
+        self._rootWdgt = wdgt
+        wdgt.setLayout(QtWidgets.QVBoxLayout())
+        wdgt.layout().setContentsMargins(0, 0, 0, 0)
+
+        self._plotManager = _RemotePlotManagerNoAsync(reqConn=self._reqPort,
+                                               repConn=self._repPort,
+                                               parentLayout=wdgt.layout(),
+                                               plotterKwargs=self._plotterKwargs)
+
+        self._win.setCentralWidget(wdgt)
+
+        flags = self._win.windowFlags()
+        flags |= QtCore.Qt.FramelessWindowHint
+        flags |= QtCore.Qt.MSWindowsFixedSizeDialogHint
+        flags |= QtCore.Qt.SubWindow
+        self._win.setWindowFlags(flags)
+
+    @classmethod
+    def createAndRun(cls, *args, **kwargs):
+        import pyqtgraph as pg
+        import sys
+        app = pg.mkQApp('RemotePlotter')
+        self = cls(*args, **kwargs)
+        sys.exit(app.exec_())
+
+
+
 
 
 class EmbeddedRemotePlotter(QtWidgets.QWidget):
@@ -318,7 +457,7 @@ class EmbeddedRemotePlotter(QtWidgets.QWidget):
 
         QtWidgets.QWidget.__init__(self)
 
-        doUseZMQ = False
+        doUseZMQ = True
 
         if doUseZMQ:
             ctx = zmq.Context()
@@ -342,7 +481,11 @@ class EmbeddedRemotePlotter(QtWidgets.QWidget):
 
         self._isReady = asyncio.Event()
 
-        self.remoteProc = mp.Process(target=_RemotePlotterApp.createAndRun,
+        if True:
+            RemotePlotterApp = _RemotePlotterApp
+        else:
+            RemotePlotterApp = _RemotePlotterAppNoAsync
+        self.remoteProc = mp.Process(target=RemotePlotterApp.createAndRun,
                                      #daemon=True,
                                      kwargs=procKwargs)
 
@@ -398,8 +541,6 @@ class EmbeddedRemotePlotter(QtWidgets.QWidget):
         else:
             self._embedWdgt = QtWidgets.QWidget.createWindowContainer(self._embedWin, parent=self)
 
-        await asyncio.sleep(1.)
-
         layout.removeWidget(tempWdgt)
         del tempWdgt
         layout.addWidget(self._embedWdgt)
@@ -442,19 +583,49 @@ class EmbeddedRemotePlotter(QtWidgets.QWidget):
         else:
             return resp
 
+    def _waitForResp(self, socket: mpc.PipeConnection | zmq.Socket):
+        """
+        Note: due to some weirdness with Qt window containering, the remote process can
+        deadlock if we don't keep processing Qt events in the main process during cursor
+        interaction with the remote plotter window. So make sure to keep processing events
+        even while waiting for an otherwise blocking result to come back.
+        """
+        if isinstance(socket, mpc.PipeConnection):
+            while not socket.poll():
+                QtWidgets.QApplication.instance().processEvents()
+                time.sleep(0.001)
+            resp = socket.recv()
+        else:
+            while True:
+                try:
+                    resp = socket.recv_pyobj(flags=zmq.NOBLOCK)
+                except zmq.ZMQError:
+                    # no message available
+                    QtWidgets.QApplication.instance().processEvents()
+                    time.sleep(0.001)
+                else:
+                    break
+        return resp
+
     def _remoteCall(self, fnStr, *args, **kwargs):
         logger.debug(f'Calling {fnStr}')
         assert self._isReady.is_set()
+
+        args = list(args)
+
+        for iArg in range(len(args)):
+            if isinstance(args[iArg], RemoteActor):
+                # convert from RemoteActor to ActorRef
+                args[iArg] = ActorRef(actorID=args[iArg].actorID)
 
         if isinstance(self._reqSocket, mpc.PipeConnection):
             self._reqSocket.send((fnStr, args, kwargs))
         else:
             self._reqSocket.send_pyobj((fnStr, args, kwargs))
         logger.debug(f'Waiting for response to {fnStr}')
-        if isinstance(self._reqSocket, mpc.PipeConnection):
-            resp = self._reqSocket.recv()
-        else:
-            resp = self._reqSocket.recv_pyobj()
+
+        resp = self._waitForResp(self._reqSocket)
+
         logger.debug(f'Handling response to {fnStr}')
         return self._handleResp(fnStr, resp)
 
@@ -467,10 +638,9 @@ class EmbeddedRemotePlotter(QtWidgets.QWidget):
         else:
             self._reqSocket.send_pyobj(('callActorMethod', actorRef, fnStr, args, kwargs))
         logger.debug(f'Waiting for response to actor.{fnStr}')
-        if isinstance(self._reqSocket, mpc.PipeConnection):
-            resp = self._reqSocket.recv()
-        else:
-            resp = self._reqSocket.recv_pyobj()
+
+        resp = self._waitForResp(self._reqSocket)
+
         logger.debug(f'Handling response to actor.{fnStr}')
         return self._handleResp('actor.' + fnStr, resp)
 
@@ -501,3 +671,6 @@ class EmbeddedRemotePlotter(QtWidgets.QWidget):
 
     def show_grid(self, *args, **kwargs):
         return self._remoteCall('show_grid', *args, **kwargs)
+
+    def remove_actor(self, *args, **kwargs):
+        return self._remoteCall('remove_actor', *args, **kwargs)
