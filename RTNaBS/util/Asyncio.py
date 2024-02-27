a1d24500623925b510d53d0549af39b8a1df9028
Chris Cline <chriscline@users.noreply.github.com>
2024-02-27 02:01:27 -0600

    Consolidate code

--- RTNaBS/util/Asyncio.py
+++ RTNaBS/util/Asyncio.py
@@ -1,9 +1,11 @@
 import asyncio
+import atexit
 import logging
 from functools import wraps
 import typing as tp
 
 from . import exceptionToStr
+from .AsyncRunner import asyncioRunAndHandleExceptions
 
 logger = logging.getLogger(__name__)
 
@@ -42,40 +44,6 @@ async def asyncTryAndLogExceptionOnError(fn: tp.Callable[..., tp.Awaitable], *ar
     except Exception as e:
         logger.error('Exception: %s' % exceptionToStr(e))
         raise e
-
-
-def asyncioRunAndHandleExceptions(fn: tp.Callable[..., tp.Awaitable], *args, **kwargs):
-    def handleExceptionInAsyncTask(loop: asyncio.AbstractEventLoop, context: dict):
-        from pprint import pprint
-        logger.error('Unhandled exception in async task, will stop loop. Exception context:\n%s' % (pprint(context),))
-        loop.stop()
-
-    loop = asyncio.get_event_loop()
-    loop.set_exception_handler(handleExceptionInAsyncTask)
-    try:
-        loop.run_until_complete(fn(*args, **kwargs))
-    except RuntimeError as e:
-        if e.args[0] == 'Event loop stopped before Future completed.':
-            logger.info('Event loop stopped early')
-        else:
-            logger.error('Exception: %s' % exceptionToStr(e))
-            raise e
-    except Exception as e:
-        logger.error('Exception: %s' % exceptionToStr(e))
-        raise e
-    finally:
-        logger.debug('Running any shutdown_asyncgens')
-        try:
-            loop.run_until_complete(loop.shutdown_asyncgens())
-        except RuntimeError as e:
-            if e.args[0] == 'Event loop is closed':
-                logger.info('Event looped already closed')
-            else:
-                raise e
-        else:
-            logger.debug('Closing loop')
-            loop.close()
-        logger.debug('Done!')
 
 
 def asyncAtomicCancellable(fn: tp.Callable[..., tp.Awaitable], *args, **kwargs):
