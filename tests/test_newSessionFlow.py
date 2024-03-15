96c394d3dd50b09be0f8440079ccb6b60bd72764
Chris Cline <chriscline@users.noreply.github.com>
2024-03-15 10:23:48 -0500

    Added setTargets test case

--- tests/test_newSessionFlow.py
+++ tests/test_newSessionFlow.py
@@ -46,6 +46,12 @@ def headModelDataSourcePath(existingResourcesDataPath):
                         'sub-test_T1Seq-SagFSPGRBRAVO_SimNIBS', 'sub-test.msh')
 
 
+@pytest.fixture
+def targetsDataSourcePath(existingResourcesDataPath):
+    return os.path.join(existingResourcesDataPath, 'testSourceData',
+                        'sub-test_ExampleTargets.json')
+
+
 @pytest.fixture(scope='session')
 def workingDir(request):
     path = request.config.cache.get('workingDir', None)
@@ -158,7 +164,7 @@ async def navigatorGUIWithoutSession() -> NavigatorGUI:
 
 @pytest.mark.asyncio
 async def test_openSession(workingDir):
-    sessionKey = 'SetHeadModel'
+    sessionKey = 'PlanFiducials'
     sessionPath = getSessionPath(workingDir, sessionKey)
     NavigatorGUI.createAndRunAsTask(sesFilepath=sessionPath)
     while True:
@@ -400,9 +406,9 @@ async def test_planFiducials(navigatorGUIWithoutSession: NavigatorGUI,
     captureScreenshot(navigatorGUI, screenshotPath)
     pyperclip.copy(str(screenshotPath))
 
-    # compareImages(screenshotPath,
-    #               os.path.join(screenshotsDataSourcePath, 'PlanFiducials_Empty.png'),
-    #               doAssertEqual=doAssertScreenshotsEqual)
+    compareImages(screenshotPath,
+                  os.path.join(screenshotsDataSourcePath, 'PlanFiducials_Empty.png'),
+                  doAssertEqual=doAssertScreenshotsEqual)
 
     # equivalent to clicking autoset button
     navigatorGUI.planFiducialsPanel._onAutosetBtnClicked(checked=False)
@@ -432,6 +438,66 @@ async def test_planFiducials(navigatorGUIWithoutSession: NavigatorGUI,
     # TODO: add additional test procedures + assertions for manually editing existing fiducials,
     #  creating new fiducials, and deleting existing fiducials
 
+
+@pytest.mark.asyncio
+@pytest.mark.order(after='test_planFiducials')
+async def test_setTargets(navigatorGUIWithoutSession: NavigatorGUI,
+                          workingDir: str,
+                          targetsDataSourcePath: str,
+                          screenshotsDataSourcePath: str):
+    navigatorGUI = navigatorGUIWithoutSession
+
+    sessionPath = copySessionFolder(workingDir, 'PlanFiducials', 'SetTargets')
+
+    # open session
+    navigatorGUI.manageSessionPanel.loadSession(sesFilepath=sessionPath)
+
+    await asyncio.sleep(1.)
+
+    # equivalent to clicking on tab
+    navigatorGUI._activateView(navigatorGUI.setTargetsPanel.key)
+
+    # give time for initialization
+    # (TODO: wait for signal to indicate tab is ready instead of waiting fixed time here)
+    await asyncio.sleep(60.)
+
+    assert navigatorGUI.activeViewKey == navigatorGUI.setTargetsPanel.key
+
+    screenshotPath = os.path.join(sessionPath, 'SetTargets_Empty.png')
+    captureScreenshot(navigatorGUI, screenshotPath)
+    pyperclip.copy(str(screenshotPath))
+
+    # compareImages(screenshotPath,
+    #               os.path.join(screenshotsDataSourcePath, 'SetTargets_Empty.png'),
+    #               doAssertEqual=doAssertScreenshotsEqual)
+
+    # equivalent to clicking "Import targets from file..." button and browsing to file
+    navigatorGUI.setTargetsPanel._importTargetsFromFile(targetsDataSourcePath)
+
+    # equivalent to clicking on corresponding entry in table
+    navigatorGUI.setTargetsPanel._tableWdgt.currentCollectionItemKey = 't2-45'
+
+    # equivalent to clicking save button
+    navigatorGUI.manageSessionPanel._onSaveSessionBtnClicked(checked=False)
+
+    ses = assertSavedSessionIsValid(sessionPath)
+
+    assert ses.targets['t2-45'].targetCoord.round(1).tolist() == [-30.1, 32.0, 52.9]
+
+    # TODO: wait for signal to indicate plots have been updated instead of waiting fixed time here
+    await asyncio.sleep(60.)
+    screenshotPath = os.path.join(sessionPath, 'SetTargets_ImportedAndSelected.png')
+    captureScreenshot(navigatorGUI, screenshotPath)
+    pyperclip.copy(str(screenshotPath))
+
+    # TODO: update ground-truth screenshot to not include a new grid target by default immediately
+    # after import (currently there due to a grid edit GUI bug that applies grid too aggressively)
+
+    compareImages(screenshotPath,
+                  os.path.join(screenshotsDataSourcePath, 'SetTargets_ImportedAndSelected.png'))
+
+    # TODO: add additional test procedures + assertions for manually editing existing targets
+    #  creating new targets, and deleting existing targets
 
 
 
