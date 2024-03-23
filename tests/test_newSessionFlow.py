8ef41de41f52d525f349b4aa9b01a044661390ac
Chris Cline <chriscline@users.noreply.github.com>
2024-03-23 12:23:21 -0500

    Add basic test case for tool import

--- tests/test_newSessionFlow.py
+++ tests/test_newSessionFlow.py
@@ -52,6 +52,12 @@ def targetsDataSourcePath(existingResourcesDataPath):
                         'sub-test_ExampleTargets.json')
 
 
+@pytest.fixture
+def toolsDataSourcePath(existingResourcesDataPath):
+    return os.path.join(existingResourcesDataPath, 'testSourceData',
+                        'ExampleTools_Minimal.json')
+
+
 @pytest.fixture(scope='session')
 def workingDir(request):
     path = request.config.cache.get('workingDir', None)
@@ -498,6 +504,56 @@ async def test_setTargets(navigatorGUIWithoutSession: NavigatorGUI,
 
     # TODO: add additional test procedures + assertions for manually editing existing targets
     #  creating new targets, and deleting existing targets
+
+
+@pytest.mark.asyncio
+@pytest.mark.order(after='test_setTargets')
+async def test_setTools(navigatorGUIWithoutSession: NavigatorGUI,
+                          workingDir: str,
+                          toolsDataSourcePath: str,
+                          screenshotsDataSourcePath: str):
+    navigatorGUI = navigatorGUIWithoutSession
+
+    sessionPath = copySessionFolder(workingDir, 'SetTargets', 'SetTools')
+
+    # open session
+    navigatorGUI.manageSessionPanel.loadSession(sesFilepath=sessionPath)
+
+    await asyncio.sleep(1.)
+
+    # equivalent to clicking on tab
+    navigatorGUI._activateView(navigatorGUI.toolsPanel.key)
+
+    # give time for initialization
+    # (TODO: wait for signal to indicate tab is ready instead of waiting fixed time here)
+    await asyncio.sleep(10.)
+
+    assert navigatorGUI.activeViewKey == navigatorGUI.toolsPanel.key
+
+    # equivalent to clicking "Import tools from file..." button and browsing to file
+    navigatorGUI.toolsPanel._importToolsFromFile(toolsDataSourcePath)
+
+    # equivalent to clicking on corresponding entry in table
+    navigatorGUI.toolsPanel._tblWdgt.currentCollectionItemKey = 'Pointer'
+
+    # equivalent to clicking save button
+    navigatorGUI.manageSessionPanel._onSaveSessionBtnClicked(checked=False)
+
+    ses = assertSavedSessionIsValid(sessionPath)
+
+    assert 'Pointer' in ses.tools
+
+    # TODO: wait for signal to indicate plots have been updated instead of waiting fixed time here
+    await asyncio.sleep(60.)
+    screenshotPath = os.path.join(sessionPath, 'SetTools_ImportedAndSelected.png')
+    captureScreenshot(navigatorGUI, screenshotPath)
+    pyperclip.copy(str(screenshotPath))
+
+    compareImages(screenshotPath,
+                  os.path.join(screenshotsDataSourcePath, 'SetTools_ImportedAndSelected.png'))
+
+    # TODO: add additional test procedures + assertions for manually editing existing tools
+    #  creating new tools, and deleting existing tools, calibrating, etc.
 
 
 
