6facb1972e07fd2e8a78a39f51394342eafb3608
Chris Cline <chriscline@users.noreply.github.com>
2024-04-04 00:20:10 -0500

    Added simulate tool position and calibrate coil test cases

--- tests/test_NavigatorGUI/test_tools.py
+++ tests/test_NavigatorGUI/test_tools.py
@@ -1,4 +1,6 @@
 import asyncio
+import jsbeautifier
+import json
 import logging
 import os
 import pyperclip
@@ -6,6 +8,7 @@ import pytest
 import shutil
 
 from RTNaBS.Navigator.GUI.NavigatorGUI import NavigatorGUI
+from RTNaBS.Navigator.GUI.ViewPanels.ToolsPanel import CoilToolWidget
 from tests.test_NavigatorGUI import utils
 from tests.test_NavigatorGUI.utils import (
     existingResourcesDataPath,
@@ -20,6 +23,18 @@ logger = logging.getLogger(__name__)
 def toolsDataSourcePath(existingResourcesDataPath):
     return os.path.join(existingResourcesDataPath, 'testSourceData',
                         'ExampleTools_Minimal.json')
+
+
+@pytest.fixture
+def simulatedPositionsPath1(existingResourcesDataPath):
+    return os.path.join(existingResourcesDataPath, 'testSourceData',
+                        'SimulatedPositions_Example1.json')
+
+
+@pytest.fixture
+def simulatedPositionsCoilCalibrationPath1(existingResourcesDataPath):
+    return os.path.join(existingResourcesDataPath, 'testSourceData',
+                        'SimulatedPositions_CoilCalibrationA1.json')
 
 
 @pytest.mark.asyncio
@@ -71,3 +86,179 @@ async def test_setTools(navigatorGUIWithoutSession: NavigatorGUI,
 
     # TODO: add additional test procedures + assertions for manually editing existing tools
     #  creating new tools, and deleting existing tools, calibrating, etc.
+
+
+@pytest.mark.asyncio
+@pytest.mark.skip(reason='For troubleshooting')
+async def test_openToolsSession(workingDir):
+    await utils.openSessionForInteraction(workingDir, 'SetTools')
+
+
+@pytest.mark.asyncio
+@pytest.mark.skip(reason='For troubleshooting')
+async def test_openSimulatedToolsSession(workingDir):
+    await utils.openSessionForInteraction(workingDir, 'SimulateTools')
+
+
+@pytest.mark.asyncio
+@pytest.mark.order(after='test_setTools')
+async def test_simulateTools(navigatorGUIWithoutSession: NavigatorGUI,
+                             workingDir: str,
+                             screenshotsDataSourcePath: str,
+                             simulatedPositionsPath1: str):
+
+    navigatorGUI = navigatorGUIWithoutSession
+
+    sessionPath = utils.copySessionFolder(workingDir, 'SetTools', 'SimulateTools')
+
+    # edit session config to enable simulated tools addon
+    # TODO: do this via GUI once have support for editing addons in GUI
+
+    addonConfig = dict()
+    addonConfig['addonInstallPath'] = '../addons/NaviNIBS_Simulated_Tools'
+    addonConfigName = 'SessionConfig_Addon_NaviNIBS_Simulated_Tools.json'
+    addonConfigPath = os.path.join(sessionPath, addonConfigName)
+    with open(addonConfigPath, 'w') as f:
+        json.dump(addonConfig, f)
+
+    baseConfigPath = os.path.join(sessionPath, 'SessionConfig.json')
+    with open(baseConfigPath, 'r+') as f:
+        baseConfig = json.load(f)
+        if 'addons' not in baseConfig:
+            baseConfig['addons'] = []
+        baseConfig['addons'].append(addonConfigName)
+
+        del baseConfig['dockWidgetLayouts']  # remove previous saved layouts since they don't include view pane for new addon
+
+        opts = jsbeautifier.default_options()
+        opts.indent_size = 2
+        beautifier = jsbeautifier.Beautifier(opts)
+        f.seek(0)
+        f.write(beautifier.beautify(json.dumps(baseConfig)))
+        f.truncate()
+
+    from addons.NaviNIBS_Simulated_Tools.Navigator.GUI.ViewPanels.SimulatedToolsPanel import SimulatedToolsPanel
+
+    # open session
+    navigatorGUI.manageSessionPanel.loadSession(sesFilepath=sessionPath)
+
+    await asyncio.sleep(1.)
+
+    # equivalent to clicking on tab
+    simulatedToolsPanel: SimulatedToolsPanel = navigatorGUI._mainViewPanels['SimulatedToolsPanel']
+    navigatorGUI._activateView(simulatedToolsPanel.key)
+
+    # give time for initialization
+    # (TODO: wait for signal to indicate tab is ready instead of waiting fixed time here)
+    await asyncio.sleep(10.)
+
+    assert navigatorGUI.activeViewKey == simulatedToolsPanel.key
+
+    await simulatedToolsPanel.importPositionsSnapshot(simulatedPositionsPath1)
+
+    await asyncio.sleep(2.)
+
+    screenshotPath = os.path.join(sessionPath, 'SimulateTools_Example1.png')
+    utils.captureScreenshot(navigatorGUI, screenshotPath)
+    pyperclip.copy(str(screenshotPath))
+
+    utils.compareImages(screenshotPath,
+                        os.path.join(screenshotsDataSourcePath, 'SimulateTools_Example1.png'),
+                        doAssertEqual=utils.doAssertScreenshotsEqual)
+
+
+@pytest.mark.asyncio
+@pytest.mark.order(after='test_simulateTools')
+async def test_calibrateCoil(navigatorGUIWithoutSession: NavigatorGUI,
+                             workingDir: str,
+                             screenshotsDataSourcePath: str,
+                             simulatedPositionsPath1: str,
+                             simulatedPositionsCoilCalibrationPath1: str):
+    navigatorGUI = navigatorGUIWithoutSession
+
+    sessionPath = utils.copySessionFolder(workingDir, 'SimulateTools', 'CalibrateCoil')
+
+    # open session
+    navigatorGUI.manageSessionPanel.loadSession(sesFilepath=sessionPath)
+
+    await asyncio.sleep(1.)
+
+    await utils.importSimulatedPositionsSnapshot(navigatorGUI, simulatedPositionsPath1)
+
+    # equivalent to clicking on tab
+    navigatorGUI._activateView(navigatorGUI.toolsPanel.key)
+
+    # give time for initialization
+    # (TODO: wait for signal to indicate tab is ready instead of waiting fixed time here)
+    await asyncio.sleep(10.)
+
+    assert navigatorGUI.activeViewKey == navigatorGUI.toolsPanel.key
+
+    # equivalent to clicking on corresponding entry in table
+    navigatorGUI.toolsPanel._tblWdgt.currentCollectionItemKey = 'Coil1'
+
+    await asyncio.sleep(10.)
+
+    # click calibrate btn
+    coilWdgt: CoilToolWidget = navigatorGUI.toolsPanel._toolWdgt
+    coilWdgt._calibrateCoilBtn.click()
+
+    await asyncio.sleep(10.)
+
+    screenshotPath = os.path.join(sessionPath, 'CalibrateCoil_1.png')
+    await utils.raiseMainNavigatorGUI()
+    utils.captureScreenshot(navigatorGUI, screenshotPath)
+    pyperclip.copy(str(screenshotPath))
+    utils.compareImages(screenshotPath,
+                        os.path.join(screenshotsDataSourcePath, 'CalibrateCoil_1.png'),
+                        doAssertEqual=utils.doAssertScreenshotsEqual)
+
+    await utils.importSimulatedPositionsSnapshot(navigatorGUI, simulatedPositionsCoilCalibrationPath1)
+
+    await asyncio.sleep(0.5)
+
+    screenshotPath = os.path.join(sessionPath, 'CalibrateCoil_2.png')
+    await utils.raiseMainNavigatorGUI()
+    utils.captureScreenshot(navigatorGUI, screenshotPath)
+    pyperclip.copy(str(screenshotPath))
+    utils.compareImages(screenshotPath,
+                        os.path.join(screenshotsDataSourcePath, 'CalibrateCoil_2.png'),
+                        doAssertEqual=utils.doAssertScreenshotsEqual)
+
+    # TODO import a simulated positions case where either coil or plate is NOT visible
+    # and assert that calibrate button is disabled
+
+    # equivalent to clicking "calibrate" button
+    coilWdgt._calibrationWindow._calibrateBtn.click()
+
+    await asyncio.sleep(0.5)
+
+    screenshotPath = os.path.join(sessionPath, 'CalibrateCoil_3.png')
+    await utils.raiseMainNavigatorGUI()
+    utils.captureScreenshot(navigatorGUI, screenshotPath)
+    pyperclip.copy(str(screenshotPath))
+    utils.compareImages(screenshotPath,
+                        os.path.join(screenshotsDataSourcePath, 'CalibrateCoil_3.png'),
+                        doAssertEqual=utils.doAssertScreenshotsEqual)
+
+    # TODO: simulate undoing calibration, closing window without changing original calibration
+
+    # equivalent to clicking close button
+    coilWdgt._calibrationWindow.wdgt.close()
+
+    await asyncio.sleep(1.0)
+
+    screenshotPath = os.path.join(sessionPath, 'CalibrateCoil_4.png')
+    await utils.raiseMainNavigatorGUI()
+    utils.captureScreenshot(navigatorGUI, screenshotPath)
+    pyperclip.copy(str(screenshotPath))
+    utils.compareImages(screenshotPath,
+                        os.path.join(screenshotsDataSourcePath, 'CalibrateCoil_4.png'),
+                        doAssertEqual=utils.doAssertScreenshotsEqual)
+
+    # equivalent to clicking save button
+    navigatorGUI.manageSessionPanel._onSaveSessionBtnClicked(checked=False)
+
+    ses = utils.assertSavedSessionIsValid(sessionPath)
+
+
