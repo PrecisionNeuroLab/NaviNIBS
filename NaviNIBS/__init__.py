59fa3e48906f2829f26e144859bb6029b8242e19
Chris Cline <chriscline@users.noreply.github.com>
2025-11-25 12:32:32 -0600

    Version 0.2.6a4

--- NaviNIBS/__init__.py
+++ NaviNIBS/__init__.py
@@ -3,5 +3,5 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s.%(msecs)03d  %(process)6d %(filename)20s %(lineno)4d %(levelname)5s: %(message)s',
                     datefmt='%H:%M:%S')
 
-__version__ = '0.2.6a3'
+__version__ = '0.2.6a4'
 
